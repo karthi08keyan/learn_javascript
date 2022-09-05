fe097a4931c072da432fdd7520f4303667a5d794
cyber_wolf <79078682+karthi08keyan@users.noreply.github.com>
2022-09-05 20:36:12 +0530

    Create decelerationtest.js

---
+++ decelerationtest.js
@@ -0,0 +1,11 @@
+function keyword(){
+  if(true){
+    var num= 10;
+    let num1=11;
+    const num2=12;
+     num1=13;/block kulla change panlam
+    console.log(num1);
+    console.log(num2);
+  }
+ num =14;
+  console.log(num);
